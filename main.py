8f59aa74c6a9a6a6315ea36f7bb377e3e932f874
wongchunkang <chunkang.wong94@gmail.com>
2021-11-14 01:44:26 +0800

    Removed unused code

--- main.py
+++ main.py
@@ -209,92 +209,6 @@ def get_client(
 # [END iot_mqtt_config]
 
 
-def parse_command_line_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(
-        description=("Example Google Cloud IoT Core MQTT device connection code.")
-    )
-    parser.add_argument(
-        "--algorithm",
-        choices=("RS256", "ES256"),
-        required=True,
-        help="Which encryption algorithm to use to generate the JWT.",
-    )
-    parser.add_argument(
-        "--ca_certs",
-        default="roots.pem",
-        help="CA root from https://pki.google.com/roots.pem",
-    )
-    parser.add_argument(
-        "--cloud_region", default="asia-east1", help="GCP cloud region"
-    )
-    parser.add_argument(
-        "--data",
-        default="Hello there",
-        help="The telemetry data sent on behalf of a device",
-    )
-    parser.add_argument("--device_id", required=True, help="Cloud IoT Core device id")
-    parser.add_argument("--gateway_id", required=False, help="Gateway identifier.")
-    parser.add_argument(
-        "--jwt_expires_minutes",
-        default=20,
-        type=int,
-        help="Expiration time, in minutes, for JWT tokens.",
-    )
-    parser.add_argument(
-        "--listen_dur",
-        default=60,
-        type=int,
-        help="Duration (seconds) to listen for configuration messages",
-    )
-    parser.add_argument(
-        "--message_type",
-        choices=("event", "state"),
-        default="event",
-        help=(
-            "Indicates whether the message to be published is a "
-            "telemetry event or a device state message."
-        ),
-    )
-    parser.add_argument(
-        "--mqtt_bridge_hostname",
-        default="mqtt.googleapis.com",
-        help="MQTT bridge hostname.",
-    )
-    parser.add_argument(
-        "--mqtt_bridge_port",
-        choices=(8883, 443),
-        default=8883,
-        type=int,
-        help="MQTT bridge port.",
-    )
-    parser.add_argument(
-        "--num_messages", type=int, default=100, help="Number of messages to publish."
-    )
-    parser.add_argument(
-        "--private_key_file", required=True, help="Path to private key file."
-    )
-    parser.add_argument(
-        "--project_id",
-        default="portsmouth-iot",
-        help="GCP cloud project name",
-    )
-    parser.add_argument(
-        "--registry_id", required=True, help="Cloud IoT Core registry id"
-    )
-    parser.add_argument(
-        "--service_account_json",
-        default=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"),
-        help="Path to service account json file.",
-    )
-
-    # Command subparser
-    command = parser.add_subparsers(dest="command")
-
-    command.add_parser("device_demo", help=mqtt_device_demo.__doc__)
-
-    return parser.parse_args()
-
 
 def event_handler(client, mqtt_topic):
     now = str(datetime.datetime.now())
@@ -387,7 +301,6 @@ def mqtt_device_demo(args):
 
 
 def main():
-    # config = parse_command_line_args()
     with open('config.json') as f:
        config_content = f.read()
     config = json.loads(config_content)
